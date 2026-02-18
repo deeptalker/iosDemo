ba56c31559f401d7798640d5d0c2d279cd813c51
deeptalker <deep@example.com>
2026-02-18 14:20:36 +0800

    Add GitHub Actions for iPhone 17 Pro , iosDemo.app

---
+++ iosDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iosDemo
+//
+//  Created by zth on 2026/2/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iosDemo/iosDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosDemoApp.swift
+//  iosDemo
+//
+//  Created by zth on 2026/2/18.
+//
+
+import SwiftUI
+
+@main
+struct iosDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
